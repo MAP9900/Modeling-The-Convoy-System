62677a029cce13a91675a5e61cfde76cf37307eb
Matthew Plambeck <142714965+MAP9900@users.noreply.github.com>
2025-02-26 18:33:29 -0500

    Delete test_rf.py

--- test_rf.py
+++
@@ -1,5 +0,0 @@
-from sklearn.ensemble import RandomForestClassifier
-
-# Try initializing a model and printing it
-rf = RandomForestClassifier(n_estimators=100, random_state=1945)
-print("Random Forest model:", rf)
